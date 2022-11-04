9b8ccdb3866f565e4823477771b6e8061bf59b7d
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2022-11-04 11:21:54 -0400

    wip orchestrator

--- r-tech-challenge-zio/src/main/scala/service/internal/Orchestrator.scala
+++ r-tech-challenge-zio/src/main/scala/service/internal/Orchestrator.scala
@@ -1,31 +1,37 @@
 package service.internal
 
+import service.entity.RecordApiEntity
 import service.external.{SourceA, SourceB}
 import zio._
 
 case class Orchestrator(sourceA: SourceA, sourceB: SourceB) {
 
-  private def triggerA(): ZIO[Any, Throwable, String] =
+  private def execute() = for {
+    queue     <- Queue.unbounded[RecordApiEntity]
+    doneTuple <- triggerA(queue) zipPar triggerB(queue)
+  } yield doneTuple
+
+  private def triggerA(queue: Queue[RecordApiEntity]): ZIO[Any, Throwable, String] =
     for {
       _      <- ZIO.logInfo("Requesting record from sourceA")
       record <- sourceA.fetchSourceARecord()
-      // put record on collection
+      _      <- queue.offer(record.get).when(!record.isEmpty && !record.get.status.equals("done"))
       done <- if (!record.isEmpty && record.get.status.equals("done")) {
                 ZIO.logInfo(s"Received done record from source A") zipRight ZIO.succeed("done")
               } else {
-                ZIO.logInfo(s"Received record from source A") zipRight triggerA()
+                ZIO.logInfo(s"Received record from source A") zipRight triggerA(queue)
               }
     } yield done
 
-  private def triggerB(): ZIO[Any, Throwable, String] =
+  private def triggerB(queue: Queue[RecordApiEntity]): ZIO[Any, Throwable, String] =
     for {
       _      <- ZIO.logInfo("Requesting record from sourceB")
       record <- sourceA.fetchSourceARecord()
-      // put record on collection
+      _      <- queue.offer(record.get).when(!record.isEmpty && !record.get.status.equals("done"))
       done <- if (!record.isEmpty && record.get.status.equals("done")) {
                 ZIO.logInfo(s"Received done record from source B") zipRight ZIO.succeed("done")
               } else {
-                ZIO.logInfo(s"Received record from source B") zipRight triggerB()
+                ZIO.logInfo(s"Received record from source B") zipRight triggerB(queue)
               }
     } yield done
 
