cf6cfeb6efdb7823963ab6726f5691917e308af2
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2022-11-08 14:49:53 +0100

    Updating processor

--- r-tech-challenge-zio/src/main/scala/service/internal/Processor.scala
+++ r-tech-challenge-zio/src/main/scala/service/internal/Processor.scala
@@ -7,33 +7,55 @@ import zio._
 
 trait Processor {}
 
-case class ProcessorImpl(recordIds: Set[String], ref: TRef[Int]) {
+case class ProcessorImpl(var recordIds: Set[String], ref: TRef[Int]) {
 
-  ///val recordIds: Set[String]
+  def process(record: RecordApiEntity): ZIO[Any, Throwable, List[RecordSubmissionApiEntity]] = {
+    val processZio: ZSTM[Any, Throwable, List[RecordSubmissionApiEntity]] = for {
+      recordsToSubmit <- processHelper(record)
+    } yield recordsToSubmit
+    processZio.commit
+  }
 
-  def process(record: RecordApiEntity) : ZIO[Any,Nothing,List[RecordSubmissionApiEntity]] =
+  private def processHelper(record: RecordApiEntity): STM[Throwable, List[RecordSubmissionApiEntity]] =
+    if (record.status.equals("ok")) {
+      if (!record.id.isEmpty) {
+        val exist = recordIds.contains(record.id.get)
+        if (exist) {
+          recordIds -= record.id.get
+          STM.attempt(List(RecordSubmissionApiEntity("joined", record.id.get)))
+        } else
+          recordIds += record.id.get
+        STM.attempt(List())
+      } else STM.attempt(List())
+    } else {
+      val count = ref.updateAndGet(_ + 1)
+      if (count == 2) {
+        STM.attempt(processOrphans())
+      } else {
+        STM.attempt(List())
+      }
+    }
+
+  def processZIO(record: RecordApiEntity): ZIO[Any, Throwable, List[RecordSubmissionApiEntity]] =
     if (record.status.equals("ok")) {
       if (!record.id.isEmpty) {
         val exist = recordIds.contains(record.id.get)
         if (exist)
-          ZIO.succeed(List(RecordSubmissionApiEntity("joined", record.id.get)))
+          ZIO.attempt(List(RecordSubmissionApiEntity("joined", record.id.get)))
         else
           recordIds += record.id.get
-        ZIO.succeed(List())
+        ZIO.attempt(List())
       } else ZIO.logInfo(s"Weird received a ${record.status} with empty id") zipRight ZIO.succeed(List())
     } else {
       val count = ref.updateAndGet(_ + 1)
-      if (count==2){
-        ZIO.logInfo(s"Process done, sending orphans") zipRight ZIO.succeed(processOrphans())
+      if (count == 2) {
+        ZIO.logInfo(s"Process done, sending orphans") zipRight ZIO.attempt(processOrphans())
       } else {
-        ZIO.logInfo(s"Received first done. Sending empty") zipRight ZIO.succeed(List())
+        ZIO.logInfo(s"Received first done. Sending empty") zipRight ZIO.attempt(List())
       }
     }
 
-  def processOrphans(): List[RecordSubmissionApiEntity] =  {
-    recordIds.map(id => RecordSubmissionApiEntity("orphan",id)).toList
-  }
-
-
+  def processOrphans(): List[RecordSubmissionApiEntity] =
+    recordIds.map(id => RecordSubmissionApiEntity("orphan", id)).toList
 
 }
